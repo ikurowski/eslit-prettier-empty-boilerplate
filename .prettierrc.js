58d257823ed6627d9edd7eb565de19339aec8e2c
ikurowski <igorkurowski@gmail.com>
2022-11-29 22:48:53 +0100

    add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+
+  module.exports = {
+    bracketSpacing: true,
+    bracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+  };
+  
+
