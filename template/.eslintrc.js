05c93ef93dba865c6bb067304ce4e0dbe6ac2f25
ikurowski <igorkurowski@gmail.com>
2022-12-23 14:26:05 +0100

    rules update

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -18,5 +18,25 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {},
+  rules: {
+    'no-multiple-empty-lines': 'error',
+    'react/prop-types': 'off',
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': 'warn',
+    'react/jsx-no-bind': 'warn',
+  },
 };
--- template/.prettierrc.js
+++ template/.prettierrc.js
@@ -1,10 +1,10 @@
-  module.exports = {
-    bracketSpacing: true,
-    bracketSameLine: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
-    endOfLine: 'auto',
-  };
-  
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
+
 
